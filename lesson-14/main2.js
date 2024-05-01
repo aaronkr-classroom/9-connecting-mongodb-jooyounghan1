4107e093b47f79ddca31eafdb6d214140c6c5826
jooyounghan1 <163230749+jooyounghan1@users.noreply.github.com>
2024-05-01 06:20:20 +0000

    Lesson 13, 14

--- lesson-14/main2.js
+++ lesson-14/main2.js
@@ -6,14 +6,26 @@ const port = 3000,
   layouts = require("express-ejs-layouts"),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  // @TODO: Subscriber 모델 가져오기
+  Subscriber = require("./models/subscriber"),
   app = express();
 
 /**
  * @TODO: Listing 14.1 (p. 205)
  * Mongoose를 사용한 MongoDB 연결
  */
+const mongoose = require("mongoose"),
+  dbURL = "mongodb+srv://2211wnd:1234@/ut-node/.034woqa.mongodb.net/?retryWrites=true&w=majority&appName=ut-node" //Atlas에서 
+  dbName ="ut-node";
 
+mongoose.connect(
+  dbURL + "/" + dbName, // URL + dbName
+  { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("DB connected!!!!");
+});
 
 /**
  * @TODO: Listing 14.2 (p. 206)
@@ -25,16 +37,52 @@ const port = 3000,
  * Listing 14.4 (p. 207)
  * 생성과 저장 구문
  */
+//version1
+let sub1 = new Subscriber({
+  name: "Aaron",
+  email:"aaronkr.trainer@ngmail.com",
+  phone: 123456789,
+  newsletter: True
+});
 
-
+sub1.save()
+  .then(savedDoc => {
+    console.log(savedDoc);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+//version2
+Subscriber.creat({
+  name: "Tom Hiddleston",
+  email:"loki@mv.com",
+  phone:66666,
+  newsletter: false
+})
+.then(savedDoc => {
+  console.log(savedDoc);
+})
+.catch(error => {
+  console.log(error);
+});
 // (선택) DB find() after 14.5
-
-
+let tom = Subscriber.findOne({name: "Tom Hiddleston"})
+                    .where(
+                      "email", /loki/
+                    );
+console.log("Found:", tom);
 /**
  * @TODO: Listing 14.6 (p. 208)
  * 데이터베이스에서 데이터 검색
  */
+var query = Subscriber.findOne({
+  name:"Aaron",
+}).where("email", /aaron/);
 
+query.exec()
+    .then((error,data) => {
+      if(data) console.log(data.name);
+    });
 
 app.set("port", process.env.PORT || port);
 app.set("view engine", "ejs");
